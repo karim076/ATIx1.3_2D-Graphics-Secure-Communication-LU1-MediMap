7b7c76190fab50bcd5f0da5dd4e17985764abd1c
Karim076 <D287878@edu.curio.nl>
2025-03-17 18:20:23 +0100

    Added login routing
    + Added Button navigation

---
+++ MediMapGame/Assets/Scripts/LoginScene/LoginManager.cs
@@ -0,0 +1,35 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoginManager : MonoBehaviour
+{
+    [Header("Camvas")]
+    public GameObject Register;
+    public GameObject Login;
+    [Header("Login Input Fields")]
+    public TMP_InputField usernameInput;
+    public TMP_InputField passwordInput;
+    [Header("Register Input Fields")]
+    public TMP_InputField usernameInputRegister;
+    public TMP_InputField passwordInputRegister;
+    public TMP_InputField emailInputRegister;
+
+    public void OnLoginClick()
+    {
+        // Go to Home Scene
+        SceneManager.LoadScene("HomeScreenScene", LoadSceneMode.Single);
+    }
+    public void OnRegisterClick()
+    {
+        // Go to Register Scene
+        Register.SetActive(true);
+        Login.SetActive(false);
+    }
+    public void ReturnToLoginPage()
+    {
+        // Go to Login Scene
+        Register.SetActive(false);
+        Login.SetActive(true);
+    }
+}
