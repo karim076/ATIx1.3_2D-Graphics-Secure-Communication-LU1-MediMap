7d3d2af49f7e98b4e940ba022b6fa0441108e672
Karim076 <D287878@edu.curio.nl>
2025-03-21 12:03:59 +0100

    Added Authentication logic

---
+++ MediMapAPI/MediMapAPI/Service/Interface/ITokenService.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Claims;
+
+namespace MediMapAPI.Service.Interface
+{
+    public interface ITokenService
+    {
+        string GenerateToken(IEnumerable<Claim> claims);
+        string GenerateRefreshToken();
+    }
+}
