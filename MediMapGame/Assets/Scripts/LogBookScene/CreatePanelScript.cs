3236dc8f2e1b44472178ddfd144e5ac7e7d04d0d
Rsulaiman06 <r.sulaiman@student.avans.nl>
2025-03-21 13:03:47 +0100

    edit log list viewer and create log

--- MediMapGame/Assets/Scripts/LogBookScene/CreatePanelScript.cs
+++ MediMapGame/Assets/Scripts/LogBookScene/CreatePanelScript.cs
@@ -1,19 +1,86 @@
+using Assets.models;
+using System;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class CreatePanelScene : MonoBehaviour
 {
+    [Header("Panels")]
     public GameObject createPanel;
     public GameObject logPanel;
+
+    [Header("Prefabs")]
+    public GameObject prefab;
+    public Transform gridContent;
+
+    [Header("Input Fields")]
+    public TMP_InputField date;
+    public TMP_InputField place;
+    public TMP_InputField note;
+
+    [Header("Canvas")]
+    public GameObject canvas;
+
+    [Header("Objects")]
+    public GameObject scrollbar;
+
+    public List<LogModel> models = new List<LogModel>();
 
     public void OpenCreatePanel()
     {
         createPanel.SetActive(true);
         logPanel.SetActive(false);
+        scrollbar.SetActive(false);
     }
+ 
 
     public void CloseCreatePanel()
     {
         createPanel.SetActive(false);
         logPanel.SetActive(true);
+        scrollbar.SetActive(true);
+    }
+
+    public void CreateNewLog()
+    {
+        var newModel = new LogModel
+        {
+            Date = date.text,
+            Place = place.text,
+            Note = note.text
+        };
+
+        models.Add(newModel);
+        LoadLogs(models);
+    }
+
+    //public void CreatePrefabInstance()
+    //{
+    //    GameObject instance = Instantiate(prefab, Vector3.zero, Quaternion.identity); //Rotatie = Vector3(0,0,0)
+    //}
+
+    public void LoadLogs(List<LogModel> models)
+    {
+        foreach(Transform child in gridContent)
+        {
+            Destroy(child.gameObject);
+        }
+
+        foreach(LogModel model in models)
+        {
+            GameObject modelItem = Instantiate(prefab, gridContent);
+            modelItem.transform.SetParent(gridContent, false);
+
+            TMP_Text dateText = modelItem.transform.Find("Date").GetComponent<TMP_Text>();
+            TMP_Text placeText = modelItem.transform.Find("Place").GetComponent<TMP_Text>();
+            TMP_Text noteText = modelItem.transform.Find("Note").GetComponent<TMP_Text>();
+
+            dateText.text = model.Date;
+            placeText.text = model.Place;
+            noteText.text = model.Note;
+        }
     }
 }
---
+++ MediMapGame/Assets/Scripts/LogBookScene/models/LogModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.models
+{
+    public class LogModel
+    {
+        public int Id { get; set; }
+        public string Date { get; set; }
+        public string Place { get; set; }
+        public string Note { get; set; }
+    }
+}
